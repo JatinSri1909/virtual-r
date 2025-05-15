fa000b3a47b892d78e75eaeed27f791fc13347b7
JatinSri1909 <jatinmavrik007@gmail.com>
2025-05-16 01:33:46 +0530

    feat:initialized the project and created navbar

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Features from "./components/Features"
+import Pricing from "./components/Pricing"
+import Testimonials from "./components/Testimonials"
+import Workflow from "./components/Workflow"
+import Footer from "./components/Footer"
+
+function App() {
+  
+  return (
+    <>
+      <Navbar />
+      <div className="pt-20 max-w-7xl mx-auto px-6">
+        <Hero />
+        <Features />
+        <Pricing />
+        <Testimonials />
+        <Workflow />
+        <Footer />
+      </div>
+    </>
+  )
+}
+
+export default App;
